d130abd7a8991c528e01d96a0dcaa6d20c03cc57
gaoxiangwu <near510@126.com>
2020-06-10 22:48:14 +0800

    学习至三角形绘制

--- Study/HelloCanvas.js
+++ Study/HelloCanvas.js
@@ -1,19 +1,16 @@
 //HelloCanvas.js
-var VSHADER_SORCE =
+var VSHADER_SORCE = 
     'attribute vec4 a_Position;\n'+
     'void main(){\n'+
         'gl_Position = a_Position;\n'+
-        'gl_PointSize = 10.0;\n'+
         '}\n';
 var FSHADER_SORCE = 
-    'precision mediump float;\n'+
-    'uniform vec4 u_FragColor;\n'+
     'void main(){\n'+
-        'gl_FragColor = u_FragColor;\n'+
+        'gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n'+
         '}\n';
 
 function main(){
-    var canvas = document.getElementById("WebGL");
+    var canvas = document.getElementById('WebGL');
     var gl = getWebGLContext(canvas);
     if(!gl){
         console.log('failed to get that the rendering context for WebGL');
@@ -21,54 +18,46 @@ function main(){
     }
 
     if(!initShaders(gl,VSHADER_SORCE,FSHADER_SORCE)){
-        console.log('failed to initialze shaders');
+        console.log('faied to initialze shaders');
         return;
     }
 
-    var a_Position = gl.getAttribLocation(gl.program,'a_Position');
-    if(a_Position<0){
-        console.log('failed to get the storage location of a_Position');
+    var n = initVertexBuffers(gl);
+    if(n<0){
+        console.log('Failed to set the positions of the vertices');
         return;
-    }
-
-    var u_FragColor = gl.getUniformLocation(gl.program,'u_FragColor');
-    if(u_FragColor<0){
-        console.log('failed to get the storage location of u_FragColor');
-        return;
-    }
-
-
-
-    canvas.onmousedown = function(ev){
-        click(ev,gl,canvas,a_Position,u_FragColor);
     }
 
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var g_points = [];
-    var g_colors = [];
-    function click(ev,gl,canvas,a_Position,u_FragColor){
-        var x = ev.clientX;
-        var y = ev.clientY;
-        var rect = ev.target.getBoundingClientRect();
-        x = ((x-rect.left)-canvas.height/2)/(canvas.height/2);
-        y = (canvas.width/2-(y-rect.top))/(canvas.width/2);
-        g_points.push([x,y]);
-        if(x>=0.0 && y>=0.0){
-            g_colors.push([1.0,0.0,0.0,1.0]);
-        }else if(x<0.0 && y<0.0){
-            g_colors.push([0.0,1.0,0.0,1.0]);
-        }else{
-            g_colors.push([0.0,0.0,1.0,1.0]);
-        }
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        for(var i = 0 ; i < g_points.length; i++){
-            var xy = g_points[i];
-            var rgba = g_colors[i];
-            gl.vertexAttrib3f(a_Position,xy[0],xy[1],0.0);
-            gl.uniform4f(u_FragColor,rgba[0],rgba[1],rgba[2],rgba[3]);
-            gl.drawArrays(gl.POINTS,0,1);  
-        }
+    gl.drawArrays(gl.LINE_LOOP,0,n);
+    
+}
+
+function initVertexBuffers(gl){
+    var vertices = new Float32Array([
+        0.0,0.5,-0.5,-0.5,0.5,-0.5
+    ]);
+
+    var n =3;
+
+    var vertexBuffer = gl.createBuffer();
+    if(!vertexBuffer){
+        console.log('Failed to create the buffer object');
+        return-1;
     }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,vertices,gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program,'a_Position');
+    if(a_Position<0){
+        console.log('failed to get the storage location of a_Position');
+        return-1;
+    }
+    gl.vertexAttribPointer(a_Position,2,gl.FLOAT,false,0,0);
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
 }
